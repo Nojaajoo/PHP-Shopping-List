d89c6bd2f84eb1d363376cd5defab7e9c3607f8d
Joona Oja <n0ojjo00@students.oamk.fi>
2021-03-05 14:39:33 +0200

    ongelmakohta

---
+++ add.php
@@ -0,0 +1,24 @@
+<?php
+require_once "inc/headers.php";
+require_once "inc/functions.php";
+
+$input = json_decode(file_get_contents("php://input"));
+$description = filter_var($input->description,FILTER_SANITIZE_STRING);
+$amount = filter_var($input->amount,FILTER_SANITIZE_NUMBER_INT);
+
+try {
+    $db = openDB();
+
+    $query = $db->prepare("insert into item(description) values (:description)");
+    $query->bindValue(":description",$description,PDO::PARAM_STR);
+    $query = $db->prepare("insert into item(amount) values (:amount)");
+    $query->bindValue(":amount",$amount,PDO::PARAM_INT);
+    $query->execute();
+
+    header("HTTP/1.1 200 OK");
+    $data = array("id" => $db->lastInsertId(),"description" => $description, "amount" => $amount);
+    echo json_encode($data);
+}
+catch (PDOException $pdoex) {
+    returnError($pdoex);
+}
